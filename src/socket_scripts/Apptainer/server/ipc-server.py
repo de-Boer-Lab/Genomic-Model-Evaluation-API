32c6395e07a244eb978b0ed4ded067ea0c4f9e1d
IshikaLuthra <ishikaluthra@ubc.ca>
2024-08-28 16:40:48 -0700

    updates

--- src/socket_scripts/Apptainer/server/ipc-server.py
+++ src/socket_scripts/Apptainer/server/ipc-server.py
@@ -4,16 +4,16 @@ import socket
 PORT = 8015       # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('172.16.47.244', PORT))
+    s.bind(('172.16.47.245', PORT))
     s.listen()
     conn, addr = s.accept()
 
-    cell_type_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    cell_type_socket.connect(('172.16.47.243', 8017))
-
-    cell_type_socket.sendall(b'Hello, cell type matcher dude!')
-    data_cell = cell_type_socket.recv(1024)
-    print(data_cell)
+    # cell_type_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # cell_type_socket.connect(('172.16.47.243', 8017))
+    #
+    # cell_type_socket.sendall(b'Hello, cell type matcher dude!')
+    # data_cell = cell_type_socket.recv(1024)
+    # print(data_cell)
     with conn:
         print('Connected by', addr)
         while True:
